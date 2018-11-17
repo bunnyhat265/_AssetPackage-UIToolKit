7e88bc3bff0be2e148066cdcf1467e253e85d9b8
Zeeshan Mohammed <zee_shan265@hotmail.com>
2018-11-16 23:49:35 -0500

    a

---
+++ Assets/_KeyBinder/Scripts/KeybinderManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Keybind {
+	public string keyName;
+	public KeyCode key;
+
+}
+
+public class KeybinderManager : MonoBehaviour {
+
+	public Keybind[] keybinds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
